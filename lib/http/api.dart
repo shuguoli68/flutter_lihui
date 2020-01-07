b6cfb227696a671df8e02000ec9bdfb9055e680d
shu <shuguoli68@163.com>
2020-01-07 10:26:10 +0800

    提交封装方法

--- lib/http/api.dart
+++ lib/http/api.dart
@@ -7,5 +7,6 @@ class Api {
   static final String baseUrl = 'http://192.168.1.198:8085';
   static final String test = '/';
   static final String login = '/login';
+  static final String register = '/register';
 
 }
